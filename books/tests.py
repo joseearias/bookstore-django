1201a50fc6a926bfbedf5a49d5bbb060580c5fc8
José Arias <j.ariascanto@gmail.com>
2022-06-23 21:54:30 -0500

    Added tests for Author model

--- books/tests.py
+++ books/tests.py
@@ -1,14 +1,20 @@
 from django.test import TestCase
 
-from .models import Book
+from .models import Author, Book
 
 
 class BookTest(TestCase):
 
     def setUp(self):
+        self.author = Author.objects.create(
+            first_name = 'JRR',
+            last_name = 'Tolkien'
+        )
+
+
         self.book = Book.objects.create(
             title='The Hobbit',
-            author='JRR Tolkien',
+            author= self.author,
             publisher='Test Publisher',
             publication_date='1985-01-01',
             price='25.00',
@@ -20,3 +26,16 @@ class BookTest(TestCase):
         self.assertEqual(f'{self.book.publisher}', 'Test Publisher')
         self.assertEqual(f'{self.book.publication_date}', '1985-01-01')
         self.assertEqual(f'{self.book.price}', '25.00')
+
+
+class AuthorTest(TestCase):
+
+    def setUp(self):
+        self.author = Author.objects.create(
+            first_name = 'JRR',
+            last_name = 'Tolkien'
+        )
+
+    def test_author_listing(self):
+        self.assertEqual(f'{self.author.first_name}', 'JRR')
+        self.assertEqual(f'{self.author.last_name}', 'Tolkien')
