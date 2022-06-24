9e2f6a279d2f1276fe91e6816d683bb4266dd39a
José Arias <j.ariascanto@gmail.com>
2022-06-23 21:45:40 -0500

    Added Author model

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Author, Book
 
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "price")
+
+admin.site.register(Author)
+admin.site.register(Book, BookAdmin)
